f3a0e13a9ca27f138065460f10d5ffffc59f8d96
Valentina <valentina.oleskeviciute@simplanova.com>
2024-10-17 10:04:16 +0300

    combine commands into one

--- extension.js
+++ extension.js
@@ -12,6 +12,7 @@ const { generalRefactoring } = require('./src/CodeRefactoring/generalRefactoring
 const { cleanupRecReference } = require('./src/CodeRefactoring/cleanupRec');
 const { cleanupPrefix } = require('./src/ProjectPreparation/cleanupPrefix');
 const { cleanupSufix } = require('./src/ProjectPreparation/cleanupSufix');
+const { changeReportsLayoutPath } = require('./src/ProjectPreparation/changeReportLayoutsPath');
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -20,6 +21,8 @@ const { cleanupSufix } = require('./src/ProjectPreparation/cleanupSufix');
  */
 function activate(context) {
 
+	let sufix = '';
+	let prefix = '';
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "project-upgrade" is now active!');
@@ -33,13 +36,32 @@ function activate(context) {
 		vscode.window.showInformationMessage(await cleanupRecReference());
 	});
 
-	let sufix = '';
-	let prefix = '';
-
-	const addApplicationAreaCommand = vscode.commands.registerCommand('extension.addApplicationAreaAll', async function () {
+	const projectPrepCommand = vscode.commands.registerCommand('extension.projectPrep', async function () {
 		await vscode.commands.executeCommand('workbench.action.closeAllGroups');
 		vscode.window.showInformationMessage(await addApplicationArea());
+
+		// Get the afix manually from user
+		const provideAfix = async () => {
+			const answer = await vscode.window.showInformationMessage('Does objects have a prefix or sufix already added?', 'No', 'Yes');
+			if (answer == 'Yes') {
+				const afix = await vscode.window.showInputBox({ prompt: 'Enter the afix to add' });
+				if (!afix) {
+					provideAfix();
+				}
+				return afix;
+			}
+			return '';
+		}
+
+		let afix = '';
+		if (sufix != '') afix = sufix;
+		else if (prefix != '') afix = prefix;
+		else afix = await provideAfix(); //if no sufix or prefix where added with extension get the afix from user
+
+		vscode.window.showInformationMessage(await renameALExtensions(afix));
+		vscode.window.showInformationMessage(await changeReportsLayoutPath());
 	});
+
 	const addSufix1Command = vscode.commands.registerCommand('extension.addSufix1', async function () {
 		await vscode.commands.executeCommand('workbench.action.closeAllGroups');
 
@@ -99,35 +121,12 @@ function activate(context) {
 		vscode.window.showInformationMessage(await cleanupPrefix(prefix));
 	});
 
-	const renameExtensionsCommand = vscode.commands.registerCommand('extension.renameExtensions', async function () {
-		await vscode.commands.executeCommand('workbench.action.closeAllGroups');
-
-		// Get the afix manually from user
-		const provideAfix = async () => {
-			const answer = await vscode.window.showInformationMessage('Does objects have a prefix or sufix already added?', 'No', 'Yes');
-			if (answer == 'Yes') {
-				const afix = await vscode.window.showInputBox({ prompt: 'Enter the afix to add' });
-				if (!afix) {
-					provideAfix();
-				}
-				return afix;
-			}
-			return '';
-		}
-
-		let afix = '';
-		if (sufix != '') afix = sufix;
-		else if (prefix != '') afix = prefix;
-		else afix = await provideAfix(); //if no sufix or prefix where added with extension get the afix from user
-
-		vscode.window.showInformationMessage(await renameALExtensions(afix));
-	});
 	const generalRefactoringCommand = vscode.commands.registerCommand('extension.generalRefactoring', async function () {
 		await vscode.commands.executeCommand('workbench.action.closeAllGroups');
 		vscode.window.showInformationMessage(await generalRefactoring());
 	});
 
-	context.subscriptions.push(addApplicationAreaCommand);
+	context.subscriptions.push(projectPrepCommand);
 	context.subscriptions.push(addRecCommand);
 	context.subscriptions.push(cleanupRecCommand);
 	context.subscriptions.push(addSufix1Command);
@@ -136,7 +135,6 @@ function activate(context) {
 	context.subscriptions.push(addPrefix1Command);
 	context.subscriptions.push(addPrefix2Command);
 	context.subscriptions.push(addPrefix3Command);
-	context.subscriptions.push(renameExtensionsCommand);
 	context.subscriptions.push(generalRefactoringCommand);
 }
 
