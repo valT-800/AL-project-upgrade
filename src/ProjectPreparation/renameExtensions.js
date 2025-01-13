c3be3145b12555afcd43db44edf026356b6dd661
Valentina Oleskeviciute <valentinaoleskeviciute@simplanova.com>
2025-01-13 10:54:00 +0200

    more options how to rename extensions

--- src/ProjectPreparation/renameExtensions.js
+++ src/ProjectPreparation/renameExtensions.js
@@ -1,6 +1,6 @@
 const { getALFiles, writeFile, getFileContent } = require('../ProjectWorkspaceManagement/workspaceMgt');
 
-module.exports.renameALExtensions = async function (/** @type {string} */ prefix, /** @type {string} */ suffix, /** @type {boolean} */ addExtMarker) {
+module.exports.renameALExtensions = async function (/** @type {string} */ prefix, /** @type {string} */ suffix, /** @type {boolean} */ addExtMarker,/** @type {boolean} */ removeSpaces) {
 
     let ALfiles = await getALFiles('');
     if (!ALfiles) return;
@@ -10,7 +10,7 @@ module.exports.renameALExtensions = async function (/** @type {string} */ prefix
     for (const file of ALfiles) {
         const fileContent = await getFileContent(file);
         if (fileContent.startsWith('tableextension ') || fileContent.startsWith('pageextension ') || fileContent.startsWith('reportextension ') || fileContent.startsWith('enumextension ')) {
-            const updatedContent = renameALExtensionObject(fileContent, prefix, suffix, addExtMarker);
+            const updatedContent = renameALExtensionObject(fileContent, prefix, suffix, addExtMarker, removeSpaces);
             if (updatedContent !== fileContent) {
                 // Write the updated content back to the file
                 await writeFile(file, updatedContent);
@@ -26,28 +26,51 @@ module.exports.renameALExtensions = async function (/** @type {string} */ prefix
  * @param {string} prefix
  * @param {string} suffix
  * @param {boolean} addExtMarker
+ * @param {boolean} removeSpaces
  */
-function renameALExtensionObject(content, prefix, suffix, addExtMarker) {
+function renameALExtensionObject(content, prefix, suffix, addExtMarker, removeSpaces) {
     // Regex pattern for AL extension objects
     const objectPattern = /\b(tableextension|pageextension|reportextension|enumextension|permissionsetextension)\s+\d+\s+("[^"]+"|\w+)\s+extends\s+("[^"]+"|\w+)/g;
 
     // Declare extension marker when function told to add extension marker
     let extMarker = '';
-    if (addExtMarker) extMarker = 'Ext';
+    if (addExtMarker) extMarker = ' Ext';
 
     let updatedContent = content.replace(objectPattern, (match, objectType, objectName, objectSource) => {
         // Create extension name from its source without spaces and characters
-        let newObjectName = objectSource.replace(/[^a-zA-Z]/g, '');
-        if (prefix != '')
-            newObjectName = `${prefix}${newObjectName}${extMarker}`;
-        else if (suffix != '')
-            newObjectName = `${newObjectName}${extMarker}${suffix}`;
-        else newObjectName = `${newObjectName}${extMarker}`;
+        let newObjectName = getExtNameFromObjSource(objectSource, prefix, suffix, extMarker);
+        if (removeSpaces)
+            newObjectName = newObjectName.replace(/[^a-zA-Z_]/g, '');
         return match.replace(objectName, newObjectName);
     });
 
     return updatedContent;
 }
+/**
+ * @param {string} objectSourceName
+ * @param {string} prefix
+ * @param {string} suffix
+ * @param {string} extMarker
+ */
+function getExtNameFromObjSource(objectSourceName, prefix, suffix, extMarker) {
 
+    let newObjectName = objectSourceName;
+    // Process extension name when object source is closed in quotes
+    if (objectSourceName.startsWith('"'))
+        newObjectName = objectSourceName.slice(1, -1);
+
+    if (prefix != '')
+        newObjectName = `"${prefix}${newObjectName}${extMarker}"`;
+    else if (suffix != '')
+        newObjectName = `"${newObjectName}${extMarker}${suffix}"`;
+    else newObjectName = `"${newObjectName}${extMarker}"`;
+
+    if (!objectSourceName.startsWith('"') && prefix != '' && suffix != '' && extMarker == '')
+        if (!prefix.includes(' ') && !suffix.includes(' '))
+            // Remove quotes from extension name that hasn't specific characters and spaces
+            newObjectName = newObjectName.slice(1, -1);
+
+    return newObjectName;
+}
 
 
